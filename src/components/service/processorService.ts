f2e222acfb9995e24e5179749a276eaa7657df8d
serje3 <serjerin322@gmail.com>
2023-01-25 20:44:55 +0300

    processor done

--- src/components/service/processorService.ts
+++ src/components/service/processorService.ts
@@ -1,12 +1,13 @@
 import {AllLoadStates, KernelState, Load, SupportState} from "../common/types";
-import {matrix, inv, multiply} from "mathjs";
+import {inv, matrix, multiply} from "mathjs";
 import {IService} from "../common/other/interfaces/IService";
 
-export class ProcessorService implements IService{
+export class ProcessorService implements IService {
     constructor(private kernels: KernelState[],
                 private loads: AllLoadStates,
                 private supports: SupportState[]) {
     }
+
     get calculate() {
         return {
             A: () => this.ACalculate(),
@@ -15,11 +16,11 @@ export class ProcessorService implements IService{
         }
     }
 
-    private getSupports(){
+    private getSupports() {
         return [this.supports.find(support => support?.leftSide), this.supports.find(support => !support?.leftSide)]
     }
 
-    private ACalculate(): number[][]{
+    private ACalculate(): number[][] {
         const A: number[][] = new Array<number[]>(this.kernels.length + 1)
             .fill([])
             .map(() => new Array<number>(this.kernels.length + 1).fill(0))
@@ -34,69 +35,70 @@ export class ProcessorService implements IService{
             const L = this.kernels[i].L
             const _A = this.kernels[i].A
             const K = (E * _A) / L
-            if (i == A.length - 1){
-                A[i][i] = K
-                return
+            console.log('K=', K, i)
+            if (i == A.length - 2) {
+                A[i + 1][i + 1] = K
             }
             A[i][i] = K + lastDiagK
             lastDiagK = K
-            A[i][i+1] = lastK?lastK:-K
-            A[i+1][i] = lastK?lastK:-K
+            console.log(lastK, -K, i, i + 1)
+
+            A[i][i + 1] = -K
+            A[i + 1][i] = -K
             lastK = K
         })
 
-        const [leftSupport,rightSupport] = this.getSupports()
+        const [leftSupport, rightSupport] = this.getSupports()
 
-        if (leftSupport){
+        if (leftSupport) {
+            A.forEach((_, i) => {
+                console.log(i)
+                A[0][i] = 0
+                A[i][0] = 0
+            })
             A[0][0] = 1
-            A[0][1] = 0
-            A[1][0] = 0
         }
 
-        if (rightSupport){
+        if (rightSupport) {
             const id = rightSupport.kernelId + 1
+            for (let i = id; i > 0; i--) {
+                A[id][i] = 0
+                A[i][id] = 0
+            }
             A[id][id] = 1
-            A[id][id - 1] = 0
-            A[id - 1][id] = 0
         }
+        console.log('A=', A)
 
         return A
     }
-    private bCalculate(): number[]{
+
+    private bCalculate(): number[] {
         const concentrated = this.loads.filter(value => value.loadType === Load.Concentrated)
         const distributed = this.loads.filter(value => value.loadType === Load.Distributed)
         const b: number[] = new Array(this.kernels.length + 1).fill(0)
         const Q: number[] = new Array(this.kernels.length).fill(0);
-        const L: number[] = [this.kernels[0].L, ...this.kernels.map(kernel => kernel.L)]
-        const F = new Array(this.kernels.length + 1).fill(0).map((_,i) => {
+        const L: number[] = [...this.kernels.map(kernel => kernel.L)]
+        const F = new Array(this.kernels.length + 1).fill(0).map((_, i) => {
             const load = concentrated.find(load => load.id === i)
-            return load?load.value:0
+            return load ? load.value : 0
         })
         for (const load of distributed) {
-            Q[load.id] += load.value
-            if (load.id !== Q.length - 1)
-                Q[load.id + 1] += load.value
+            Q[load.id] = load.value
         }
-        for (let i = 0; i <b.length; i++) {
-            if (i==0){
-                // console.log(b, 'F=',F[i],'Q=', Q[i], 'L=',L[i], 'для i=',i)
-                b[i] = F[0] + Q[0] * L[0] / 2
-                b[i + 1] = Q[0] + L[0] / 2
-                // console.log('после = ',b)
-            } else if(i === this.kernels.length){
-                // console.log(b, 'F=',F[i],'Q=', Q[i-1], 'L=',L[i], 'для i=',i)
-                b[i] = F[i]
-                b[i] += Q[i-1] * L[i] / 2
-                // console.log('после = ',b)
+        console.log('Q', Q, L)
+        for (let i = 0; i < b.length; i++) {
+
+            if (i === 0) {
+                b[i] = F[i] + Q[i] * L[0] / 2
+            } else if (i === b.length - 1) {
+                b[i] = F[i] + Q[i - 1] * L[i - 1] / 2
             } else {
-                // console.log(b, 'F=',F[i],'Q=', Q[i], 'L=',L[i], 'для i=',i)
-                b[i] = F[i] + Q[i] * L[i] / 2
-                b[i + 1] = Q[i] * L[i] / 2
-                // console.log('после = ',b)
+                b[i] = F[i] + (Q[i - 1] * L[i - 1] / 2) + (Q[i] * L[i] / 2)
             }
+            console.log(b)
         }
 
-        const [left,right] = this.getSupports()
+        const [left, right] = this.getSupports()
         if (left)
             b[0] = 0
         if (right)
@@ -107,12 +109,12 @@ export class ProcessorService implements IService{
 
     private deltaCalculate(): number[] {
         const A = this.calculate.A()
-        const b = this.calculate.b()
-        console.log(A)
+        const b = matrix(this.calculate.b())
+        console.log(A, b)
         const inverse = inv(matrix(A))
-        const x = multiply(inverse,b)
+        const x = multiply(inverse, b)
         const result = []
-        x.forEach(v=>result.push(v))
+        x.forEach(v => result.push(v))
         return result
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 import styles from '../styles/Home.module.css';
-import CanvasCoordinatesComponent from "../components/index/CanvasCoordinatesComponent";
-import {getNewPos} from "../components/common/utils/getNewPos";
-import {dotsArray} from "../test_data/dots";
-import {lines} from "../test_data/lines";
-import {Point} from "../components/common/types";
-import {Edges} from "../components/common/edges/Edges";
-import {Lines} from "../components/common/lines/Lines";
 import Link from "next/link";
 
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            <div className="text-3xl">SAPRbar 4.0</div>
+            <div className="text-3xl">ТОЧНО НЕ SAPRbar 4.0</div>
             <div className={styles.description}>Работа Ерина Сергея ИДБ-20-11</div>
             <Link href={"/preprocessor"} className={styles.card}>
                 <h3 className={""}>Препроцессор</h3>
